d8617606c088b4d4cdb38ac77c46cbb9c43f6b10
atapetrone <taglepetrone@gmail.com>
2020-05-28 12:53:11 -0500

    Se edito y modifico el css de ejemplo mediante javascript

--- 002 DOM-y-BOM/script2.js
+++ 002 DOM-y-BOM/script2.js
@@ -6,7 +6,13 @@ let h1Elem= document.querySelector("h1");
 console.log(h1Elem);
 let aElem= document.querySelectorAll("a");
 //console.log(aElem.length);
-
+let pElem = document.querySelector("p"); //Estas agarrando solo el primero
+console.log(pElem);
+let secElem = document.querySelector("section");
+console.log(secElem);
+let butElem = document.querySelector("button");
+console.log(butElem);
+let footElem = document.querySelector("footer");
 
 
 bodyElem.style.backgroundColor = "#fffbf0";
@@ -54,3 +60,57 @@ aElem[i].style.color = "#38488f";
 aElem[i].style.textDecoration="none";
 
 };
+
+
+
+secElem.style.width = "800px"
+secElem.style.margin = "5em auto"
+secElem.style.padding = "2em"
+//secElem.style.backgroundColor = "#fdfdff"
+secElem.style.borderRadius = "0.5em"
+secElem.style.boxShadow = "2px 3px 7px 2px rgba(0,0,0,0.08)"
+
+/*  ESTO SE COMENTA PARA VER UN EJEMPLO DE COMO INYECTAR CSS
+pElem.style.color = "gray";
+pElem.style.fontSize ="21px";
+pElem.style.maxWidth = "800px";
+pElem.style.textAlign ="center";
+*/
+
+ 
+//PARA EL EJMPLO VAMOS A style.css Y CREAMOS LOS MISMOS ELEMENTOS DE ESTILO. Y AHORA ASIGNAMOS CON classList.add "un mensaje"
+pElem.classList.add("un-mensaje");
+
+//TAMBIEN PODEMOS ELIMINAR LA CLASE:
+//pElem.classList.remove("un-mensaje"); 
+
+// Aquí otro ejemplo de remove:
+secElem.classList.remove("remover")
+// puedes checar la consola para verificar que ya no existe a pesar de que esta clase se dio de alta directamente en el html
+
+//TOGGLE
+//.toggle sirve para hacer un switch de encendido apagado de una clase... si no esta lo crea, si esta lo elimina
+
+
+//Vamos a centrar el boton que se encuentra dentro de la seccion "footer"
+footElem.classList.add("centro");
+
+//EJEMPLO DE .toggle:
+butElem.onclick= function(){ //Para este ejemplo estamos inyectando onclick al boton y asignando una funcion
+secElem.classList.toggle ("on");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
