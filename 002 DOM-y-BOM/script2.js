f3a61dabc7f2f1143de61a11956c3093d79139aa
atapetrone <taglepetrone@gmail.com>
2020-05-28 19:18:17 -0500

    Aprendimos a modificar los attributes con javascript

--- 002 DOM-y-BOM/script2.js
+++ 002 DOM-y-BOM/script2.js
@@ -5,7 +5,7 @@ console.log(divElem);
 let h1Elem= document.querySelector("h1");
 console.log(h1Elem);
 let aElem= document.querySelectorAll("a");
-//console.log(aElem.length);
+console.log(aElem.length);
 let pElem = document.querySelector("p"); //Estas agarrando solo el primero
 console.log(pElem);
 let secElem = document.querySelector("section");
@@ -13,6 +13,10 @@ console.log(secElem);
 let butElem = document.querySelector("button");
 console.log(butElem);
 let footElem = document.querySelector("footer");
+
+let ulElem = document.querySelector("ul");
+let marcaButElem= document.querySelector("#IDmarcaBut");
+
 
 
 bodyElem.style.backgroundColor = "#fffbf0";
@@ -66,7 +70,6 @@ aElem[i].style.textDecoration="none";
 secElem.style.width = "800px"
 secElem.style.margin = "5em auto"
 secElem.style.padding = "2em"
-//secElem.style.backgroundColor = "#fdfdff"
 secElem.style.borderRadius = "0.5em"
 secElem.style.boxShadow = "2px 3px 7px 2px rgba(0,0,0,0.08)"
 
@@ -76,8 +79,6 @@ pElem.style.fontSize ="21px";
 pElem.style.maxWidth = "800px";
 pElem.style.textAlign ="center";
 */
-
- 
 //PARA EL EJMPLO VAMOS A style.css Y CREAMOS LOS MISMOS ELEMENTOS DE ESTILO. Y AHORA ASIGNAMOS CON classList.add "un mensaje"
 pElem.classList.add("un-mensaje");
 
@@ -90,27 +91,39 @@ secElem.classList.remove("remover")
 
 //TOGGLE
 //.toggle sirve para hacer un switch de encendido apagado de una clase... si no esta lo crea, si esta lo elimina
-
-
-//Vamos a centrar el boton que se encuentra dentro de la seccion "footer"
+//Antes vamos a centrar el boton que se encuentra dentro de la seccion "footer"
 footElem.classList.add("centro");
-
 //EJEMPLO DE .toggle:
 butElem.onclick= function(){ //Para este ejemplo estamos inyectando onclick al boton y asignando una funcion
 secElem.classList.toggle ("on");
 };
 
 
+//ATTRIBUTE
+// A continuación se explica como acceder a los atributos de los elementos
+console.log(marcaButElem);
+let attMarcaBut =marcaButElem.getAttribute("onclick");
+console.log(attMarcaBut);
+let idMarcaBut =marcaButElem.getAttribute("id");
+console.log(idMarcaBut);
+//AHORA VAMOS A CAMBIAR EL ID ANTES CONSULTADO Y TAMBIEN agregaremos un CLASS
+marcaButElem.setAttribute("id", "IDlogoBut1 IDlogoBut2 IDlogoBut3");
+marcaButElem.setAttribute("class", "marcaClass");
+idMarcaBut =marcaButElem.getAttribute("id");
+let classMarcaBut =marcaButElem.getAttribute("class");
+console.log(idMarcaBut);
+console.log(classMarcaBut);
+// Ahora cambiaremos la clase que acabamos de crear a algo diferente
+marcaButElem.setAttribute("class", "marcaClass8888888"); //AQUI CARGAS EL CAMBIO
+classMarcaBut =marcaButElem.getAttribute("class"); //YA LO ASIGNASTE PERO AQUI HAY QUE ACTUALIZAR A LA VARIABLE PARA PODER DESPUES DESPLEGARLO CON UN CONSOLE.LOG
+console.log(classMarcaBut);
 
 
-
-
-
-
-
-
-
-
-
-
-
+//POR ULTIMO APRENDEREMOS A BORRAR UN ATTRIBUTE
+// COMO EJEMPLO BORRAREMOS EL ATTRIBUTE ONCLICK
+/* 
+marcaButElem.removeAttribute("onclick");   //   Esta es la manera de borrar el attribute "onclick"
+marcaButElem.removeAttribute("id");         // Esta seria la manera de borrar el attribute "id"
+marcaButElem.removeAttribute("class");         // Esta seria la manera de borrar el attribute "id"
+marcaButElem.innerText = "Hola Mundo"; //Este no borra, solo se me antojo cambiar el texto
+ */
