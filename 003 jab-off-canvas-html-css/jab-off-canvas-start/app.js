38f7353e8eefbed0f355d19be2b1abd7a5daceb3
atapetrone <taglepetrone@gmail.com>
2020-06-13 13:42:51 -0500

    ultimos detalles del ejercicio y comentarios

--- 003 jab-off-canvas-html-css/jab-off-canvas-start/app.js
+++ 003 jab-off-canvas-html-css/jab-off-canvas-start/app.js
@@ -10,20 +10,16 @@ console.log(contenido); */
 
 
 hamburguesa.addEventListener("click", function () {
-
 columna.classList.toggle("open");
 contenido.classList.toggle("shift");
-cruz();
+cruz(); //aqui llamo a la función que cambia el css de las 3 lineas de la hamburguesa (ver abajo)
 });
 
-
 //console.log(elementosHamburguesa);
-
-
  
 function cruz(){
   for(let guion of elementosHamburguesa){
-     guion.classList.toggle("change");
+     guion.classList.toggle("change"); //Checate que forma tan elegante de manejar un CSS
   };
 };
 
